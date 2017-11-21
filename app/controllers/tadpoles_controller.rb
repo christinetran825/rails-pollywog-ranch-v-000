728c5de51297562cb0fc7692c22637b74b4c8f9f
Christine Tran <christinetran825@gmail.com>
2017-11-21 22:30:21 +0000

    automatically backed up by learn

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -4,7 +4,6 @@ class TadpolesController < ApplicationController
   # add your metamorphosize action here
 
   def metamorphose
-    @frog = Frog.new
     @tadpole = Tadpole.find(params[:id])
     @frog.name = @tadpole.name
     @frog.color = @tadpole.color
